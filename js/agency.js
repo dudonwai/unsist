c8a3588a4b4b6119a2e680f2ed873d98c4c05156
Dudon Wai <dudonwai@gmail.com>
2017-03-15 23:29:38 -0600

    Updating Index

--- js/agency.js
+++ js/agency.js
@@ -7,7 +7,7 @@
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 0)
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -15,7 +15,7 @@
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
-        offset: -1
+        offset: 51
     });
 
     // Closes the Responsive Menu on Menu Item Click
